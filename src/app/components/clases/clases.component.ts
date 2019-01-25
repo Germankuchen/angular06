ba079ecd6619684d15cea2ba04656af9bb43befa
germankuchen <germankuchen@gmail.com>
2019-01-25 13:50:27 -0300

    the last

---
+++ src/app/components/clases/clases.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html'
+})
+export class ClasesComponent implements OnInit {
+
+  alert = 'alert-danger';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/css/css.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-css',
+  template: `
+    <p>
+      css works!
+    </p>
+  `,
+  styles: [`p { color:red}`]
+})
+export class CssComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
